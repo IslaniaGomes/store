0694c5f29e1306f7e48b29ca664725d2a52e8ca0
Islânia Gomes <163878926+IslaniaGomes@users.noreply.github.com>
2024-12-09 20:09:53 -0300

    up

---
+++ src/main.js
@@ -0,0 +1,9 @@
+
+function clickBurguer() {
+    const itens = document.getElementById('itens'); 
+    if (itens.style.display === 'block') {
+        itens.style.display = 'none';
+    } else {
+        itens.style.display = 'block';
+    }
+}

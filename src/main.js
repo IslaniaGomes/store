1ac397d5277f2bca736d7e53e34a978b66de7dc6
Islânia Gomes <163878926+IslaniaGomes@users.noreply.github.com>
2024-12-13 19:57:45 -0300

    Adicionado lightbox

--- src/main.js
+++ src/main.js
@@ -9,3 +9,47 @@ function clickBurguer() {
 }
 
 
+// Open lightbox
+function openLightbox(imageSrc) {
+
+    // Display lightbox element
+    document.getElementById('lightbox').style.display = 'block';
+    document.getElementById('lightbox-image').src = imageSrc;
+  }
+  
+  // Close lightbox
+  function closeLightbox() {
+  
+    document.getElementById('lightbox').style.display = 'none';
+  
+  }
+  
+  
+  // Image slide index
+  let slideIndex = 0;
+  
+  // Display slide at passed index  
+  function showSlide(n) {
+    slideIndex = n;   
+    displaySlide();
+  }
+  
+  // Advance to next slide
+  function plusSlides(n) {
+    slideIndex += n;
+    displaySlide();  
+  }
+  
+  function displaySlide() {
+  
+    // Get slide images
+    let slides = document.getElementsByClassName('lightbox-slide');  
+  
+    // Hide all slides 
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";   
+    }
+  
+    // Show one slide based on index  
+    slides[slideIndex].style.display = "block";  
+  }
